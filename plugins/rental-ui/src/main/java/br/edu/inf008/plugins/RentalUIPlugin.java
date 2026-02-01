82c21c902f0b6e6bdb1a45327bf5a7ab0d74e0c6
Angelito Chagas <chagasangelito@gmail.com>
2026-02-01 18:06:06 -0300

    feat(rental-ui): add rental UI plugin with vehicle type calculation

--- plugins/rental-ui/src/main/java/br/edu/inf008/plugins/RentalUIPlugin.java
+++ plugins/rental-ui/src/main/java/br/edu/inf008/plugins/RentalUIPlugin.java
@@ -1,30 +1,83 @@
 package br.edu.ifba.inf008.plugins;
 
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuItem;
+import javafx.application.Platform;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
-import javafx.scene.Node;
 
 import br.edu.ifba.inf008.interfaces.IPlugin;
+import br.edu.ifba.inf008.interfaces.ICore;
 import br.edu.ifba.inf008.interfaces.IUIController;
-import br.edu.ifba.inf008.shell.Core;
+import br.edu.ifba.inf008.interfaces.domain.IVehicleType;
+import br.edu.ifba.inf008.interfaces.domain.IVehicleTypeRegistry;
 
 public class RentalUIPlugin implements IPlugin {
-    
+
     @Override
-    public void init() {
-        IUIController ui = Core.getInstance().getUIController();
+    public boolean init() {
 
-        Label label = new Label("Rental UI loaded successfully!");
-        VBox content = new VBox(label);
+        Platform.runLater(this::buildUI);
 
-        ui.createTab("Rental", content);
+        return true;
+    }
 
-        MenuItem menuItem = ui.createMenuItem("Rental", "New Rental");
+    private void buildUI() {
+        IUIController ui = ICore.getInstance().getUIController();
+        IVehicleTypeRegistry registry = ICore.getInstance().getVehicleTypeRegistry();
 
-        menuItem.setOnAction(e -> {
-            label.setText("New Rental clicked!");
-            System.out.println("Rental menu clicked");
+        VBox root = new VBox(10);
+
+        Label title = new Label("New Rental");
+
+        ComboBox<String> vehicleTypeCombo = new ComboBox<>();
+        vehicleTypeCombo.getItems().addAll(
+                "ECONOMY", "COMPACT", "SUV", "LUXURY", "VAN", "ELECTRIC"
+        );
+
+        TextField dailyRateField = new TextField();
+        dailyRateField.setPromptText("Daily rate");
+
+        TextField daysField = new TextField();
+        daysField.setPromptText("Number of days");
+
+        Label totalLabel = new Label("Total: -");
+
+        Button calculate = new Button("Calculate total");
+
+        calculate.setOnAction(e -> {
+            String typeName = vehicleTypeCombo.getValue();
+
+            if (typeName == null) {
+                totalLabel.setText("Select a vehicle type");
+                return;
+            }
+
+            IVehicleType type = registry.getByName(typeName);
+            if (type == null) {
+                totalLabel.setText("Vehicle type not registered");
+                return;
+            }
+
+            try {
+                double dailyRate = Double.parseDouble(dailyRateField.getText());
+                long days = Long.parseLong(daysField.getText());
+
+                double total = type.calculateTotal(dailyRate, days);
+                totalLabel.setText("Total: " + total);
+
+            } catch (NumberFormatException ex) {
+                totalLabel.setText("Invalid values");
+            }
         });
+
+        root.getChildren().addAll(
+                title,
+                vehicleTypeCombo,
+                dailyRateField,
+                daysField,
+                calculate,
+                totalLabel
+        );
+
+        ui.createTab("Rental", root);
     }
 }
