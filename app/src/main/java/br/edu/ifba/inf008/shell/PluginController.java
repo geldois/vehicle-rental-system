9956995965b5269e5133d594c3a07d47ad384f1e
Angelito Chagas <chagasangelito@gmail.com>
2026-02-02 22:37:56 -0300

    build: enable ServiceLoader plugins with shaded fat jar

--- app/src/main/java/br/edu/ifba/inf008/shell/PluginController.java
+++ app/src/main/java/br/edu/ifba/inf008/shell/PluginController.java
@@ -15,59 +15,25 @@ public class PluginController implements IPluginController {
     @Override
     public boolean init() {
         try {
-            File pluginsDir = new File("./plugins");
+            java.util.ServiceLoader<IPlugin> loader =
+                    java.util.ServiceLoader.load(IPlugin.class);
 
-            if (!pluginsDir.exists() || !pluginsDir.isDirectory()) {
-                System.out.println("Diretório ./plugins não encontrado.");
-                return true;
+            boolean found = false;
+
+            for (IPlugin plugin : loader) {
+                found = true;
+                plugin.init();
+                System.out.println("Plugin carregado: " + plugin.getClass().getName());
             }
 
-            File[] jars = pluginsDir.listFiles((dir, name) -> name.endsWith(".jar"));
-
-            if (jars == null || jars.length == 0) {
+            if (!found) {
                 System.out.println("Nenhum plugin encontrado.");
-                
-                return true;
-            }
-
-            for (File jar : jars) {
-                URL jarUrl = jar.toURI().toURL();
-                URLClassLoader loader = new URLClassLoader(
-                        new URL[]{jarUrl},
-                        App.class.getClassLoader()
-                );
-
-                try (java.util.jar.JarFile jarFile = new java.util.jar.JarFile(jar)) {
-                    jarFile.stream()
-                            .filter(e -> e.getName().endsWith(".class"))
-                            .forEach(entry -> {
-                                try {
-                                    String className = entry.getName()
-                                            .replace("/", ".")
-                                            .replace(".class", "");
-
-                                    Class<?> _class = Class.forName(className, true, loader);
-
-                                    if (IPlugin.class.isAssignableFrom(_class)) {
-                                        IPlugin plugin = (IPlugin) _class
-                                                .getDeclaredConstructor()
-                                                .newInstance();
-
-                                        plugin.init();
-                                        System.out.println("Plugin carregado: " + _class.getName());
-                                    }
-
-                                } catch (Exception ignored) {
-                                }
-                            });
-                }
             }
 
             return true;
 
         } catch (Exception e) {
             e.printStackTrace();
-            
             return false;
         }
     }
